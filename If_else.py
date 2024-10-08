df095bb493330e57fea416697a8e848966432b6e
Vinod Chauhan <136825472+vinodchauhan00@users.noreply.github.com>
2024-10-08 20:29:10 +0530

    Update If_else.py

--- If_else.py
+++ If_else.py
@@ -1,1 +1,9 @@
-print("RAm Ram ji")
+num=int(input("Enter your grade:"))
+if num>=90:
+  print("Your grade A:")
+elif num>=70:
+ print ("Your grade B:")
+elif num>=50:
+ print("Your grade C:")
+else:
+ print("Invalid grade")
